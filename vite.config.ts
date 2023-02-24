f67c7b1f19b2caee2648a6f9ffda44ba50ea7f74
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2023-02-24 09:46:04 -0600

    Add rollup-plugin-polyfill-node to fix production build
    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: { global: "window", "process.env": {} },
+  build: {
+    rollupOptions: { plugins: [nodePolyfills()] },
+    commonjsOptions: { transformMixedEsModules: true },
+  },
+  define: { global: "globalThis" },
   server: { open: true, port: 3000 },
   optimizeDeps: {
     esbuildOptions: {
